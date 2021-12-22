4385b5cb5ac9080ad9cb62288d609fd32bbc767a
rubyfriedman <rfriedman30@stuy.edu>
2021-12-21 21:05:00 -0500

    updates

--- SearchDriver.java
+++ SearchDriver.java
@@ -1,35 +1,36 @@
 public class SearchDriver {
-	private static double avgBin = 0.0;
-	private static double avgLin = 0.0;
+	private static long avgBin = 0;
+	private static long avgLin = 0;
 	private static long begTimeLin = 0;
 	private static long begTimeBin = 0;
 	private static long endTimeLin = 0;
 	private static long endTimeBin = 0;
 	private static long timeLin = 0;
 	private static long timeBin =0;
+	private static long sumLin =0;
+	private static long sumBin =0;
 
 	public static void averageTime(Comparable[] a) {
 		for (int i = 0; i < 200; i++) {
-			int target = a.length;
+			int target = a.length-1;
 			begTimeLin = System.currentTimeMillis();
                		LinSearch.linSearch(a, target);
                 	endTimeLin = System.currentTimeMillis();
                 	timeLin = endTimeLin - begTimeLin;
-			avgLin += timeLin;
+			sumLin += timeLin;
 
 	                begTimeBin = System.currentTimeMillis();
 	                BinSearch.binSearch(a, target);
 	                endTimeBin = System.currentTimeMillis();
         	        timeBin = endTimeBin - begTimeBin;
-			avgBin += timeBin;
+			sumBin += timeBin;
 		}
 		avgLin /= 200;
 		avgBin /= 200;
 		System.out.println("\n");
 		System.out.println(""+a.length+" length arrays: ");
-		System.out.println("linSearch took an average of " + avgLin + " milliseconds.");
-		System.out.println("However, binSearch took an avg of " + avgBin + " milliseconds.");
-
+                System.out.println("For 200 trials linSearch took a total of " + sumLin + " milliseconds.");
+		System.out.println("However, for 200 trials binSearch took a total of " + sumBin + " milliseconds.");
 	}
 
 	public static Comparable[] populate(Comparable[] x) {
